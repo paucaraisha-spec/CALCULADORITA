e15e9ae86b657f05d06c080ed8f5dab9a77692a2
paucaraisha-spec <paucaraisha@gmail.com>
2026-07-22 15:32:14 -0500

    Update calculadora.py
    AGREGAR LA RESTA                                                                                                        Amira Gomez

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,21 @@
 ## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
+# Autor(a): AMIRA GOMEZ
 # ===========================================
+print("CALCULADORA")
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n 1. Sumar")
+print(" 2. Restar")
 
-print("Resultado:", suma)
+opcion = input("\n Seleccione una opción: ")
+
+if opcion == "1":
+    print("\n La suma es:", num1 + num2)
+
+elif opcion == "2":
+    print("\n La resta es:", num1 - num2)
+
+else:
+    print("\n Opción no válida.")
